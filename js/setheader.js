daa14183c4225e02f2d863a3e867f8fe457ed7d7
Sandy <star930104@gmail.com>
2018-05-10 13:06:26 -0700

    finishing intro

---
+++ js/setheader.js
@@ -0,0 +1,37 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+var h = header.scrollHeight;
+var cumulativeOffset = function (element) {
+    var top = 0, left = 0;
+    do {
+        top += element.offsetTop || 0;
+        left += element.offsetLeft || 0;
+        element = element.offsetParent;
+    } while (element);
+
+    return {
+        top: top,
+        left: left
+    };
+};
+function jumpToPosition(id) {
+    var e = document.getElementById(id);
+    var dest = cumulativeOffset(e);
+    dest.top -= h;
+    window.scrollTo(dest.left, dest.top);
+}
